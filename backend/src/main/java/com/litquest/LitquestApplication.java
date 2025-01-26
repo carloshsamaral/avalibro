ff8746950721aff0971c528276260facdf5f6174
Carlos Amaral <carloshsamaral@MacBook-Pro-de-Carlos.local>
2025-01-26 00:39:08 -0300

    first commit

---
+++ backend/src/main/java/com/litquest/LitquestApplication.java
@@ -0,0 +1,13 @@
+package com.litquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitquestApplication.class, args);
+	}
+
+}
